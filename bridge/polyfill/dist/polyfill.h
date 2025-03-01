64434a2ad2a05f4eb52b78b617bfd1d04dbe0f3b
Marvin Cano <canomarvin01@gmail.com>
2025-03-01 12:20:13 +0100

    build: added polyfill.cc and polyfill.h, flutter doesn't build them automatically when using webf from git url.

---
+++ bridge/polyfill/dist/polyfill.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2022-present The WebF authors. All rights reserved.
+ */
+#ifndef POLYFILL_H
+#define POLYFILL_H
+
+#include "core/executing_context.h"
+
+void initWebFPolyFill(webf::ExecutingContext *context);
+
+#endif // POLYFILL_H
---
+++ bridge/polyfill/dist/testframework.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2022-present The WebF authors. All rights reserved.
+ */
+#ifndef TESTFRAMEWORK_H
+#define TESTFRAMEWORK_H
+
+#include "core/executing_context.h"
+
+void initWebFTestFramework(webf::ExecutingContext *context);
+
+#endif // TESTFRAMEWORK_H
